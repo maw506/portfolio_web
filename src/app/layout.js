d3dcd0b415199f8eefa6a660f7c42ed84bcb1a4e
maw506 <gigenamauro@gmal.com>
2024-02-27 02:10:46 -0300

    paleta de colores

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,16 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`container text-center`}>{children}</body>
+    <html lang="en es">
+      <Head>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <body className="container">{children}</body>
     </html>
   );
 }
